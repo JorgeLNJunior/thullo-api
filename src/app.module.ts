59ef9b168874cc330695deaa0279d3ed644d1c8a
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-08-11 19:25:06 -0300

    build: adicionar o módulo de rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common'
 import { AuthModule } from './app/modules/auth/auth.module'
 import { ConfigModule } from './modules/config.module'
 import { PrismaModule } from './modules/prisma.module'
+import { RateLimitModule } from './modules/rateLimit.module'
 
 @Module({
-  imports: [ConfigModule, PrismaModule, AuthModule]
+  imports: [ConfigModule, PrismaModule, AuthModule, RateLimitModule]
 })
 export class AppModule {}
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { Throttle } from '@nestjs/throttler'
 
 import { AuthService } from './auth.service'
 import { RegisterUserDto } from './dto/registerUser.dto'
@@ -7,6 +8,7 @@ import { RegisterUserDto } from './dto/registerUser.dto'
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Throttle(10, 60)
   @Post('register')
   registerUser(@Body() dto: RegisterUserDto) {
     return this.authService.registerUser(dto)
