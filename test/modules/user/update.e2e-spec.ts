7d604dc35abb53c6144d9c8ded10ed9f299458a5
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-08-17 11:20:10 -0300

    test(e2e): criar a rota de atualizar usuários

---
+++ test/modules/user/update.e2e-spec.ts
@@ -0,0 +1,141 @@
+import { faker } from '@faker-js/faker'
+import { RegisterUserDto } from '@modules/auth/dto/registerUser.dto'
+import { UserEntity } from '@modules/user/docs/user.entity'
+import { UpdateUserDto } from '@modules/user/dto/updateUser.dto'
+import { ValidationPipe } from '@nestjs/common'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppModule } from '@src/app.module'
+import { useContainer } from 'class-validator'
+import { PrismaService } from 'nestjs-prisma'
+import { randomUUID } from 'node:crypto'
+
+import { generateAccessToken } from '../../helpers/auth.helper'
+
+describe('UserController/update (e2e)', () => {
+  let app: NestFastifyApplication
+  let prisma: PrismaService
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter()
+    )
+    prisma = app.get(PrismaService)
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        forbidNonWhitelisted: true,
+        whitelist: true
+      })
+    )
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
+    await app.init()
+    await app.getHttpAdapter().getInstance().ready()
+  })
+
+  afterEach(async () => {
+    await app.close()
+  })
+
+  it('/users (PATCH) Should update a user', async () => {
+    const data: RegisterUserDto = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(randomUUID()),
+      password: faker.internet.password(6)
+    }
+    const body: UpdateUserDto = {
+      name: faker.internet.userName()
+    }
+
+    const user = await prisma.user.create({ data: data })
+
+    const token = generateAccessToken(user)
+
+    const result = await app.inject({
+      method: 'PATCH',
+      path: `/users/${user.id}`,
+      payload: body,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    console.log(result.json())
+
+    expect(result.statusCode).toBe(200)
+    expect(result.json()).toMatchObject(UserEntity.prototype)
+  })
+
+  it('/users (PATCH) Should return 400 if the user is is invalid', async () => {
+    const data: RegisterUserDto = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(randomUUID()),
+      password: faker.internet.password(6)
+    }
+    const body: UpdateUserDto = {
+      name: faker.internet.userName()
+    }
+
+    const user = await prisma.user.create({ data: data })
+
+    const token = generateAccessToken(user)
+
+    await prisma.user.delete({ where: { id: user.id } })
+
+    const result = await app.inject({
+      method: 'PATCH',
+      path: `/users/${user.id}`,
+      payload: body,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    expect(result.statusCode).toBe(400)
+    expect(result.json().message[0]).toBe('invalid user id')
+  })
+
+  it('/users (PATCH) Should return 403 if the user has no access rights', async () => {
+    const data: RegisterUserDto = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(randomUUID()),
+      password: faker.internet.password(6)
+    }
+    const unauthorizedUserdata: RegisterUserDto = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(randomUUID()),
+      password: faker.internet.password(6)
+    }
+    const body: UpdateUserDto = {
+      name: faker.internet.userName()
+    }
+
+    const user = await prisma.user.create({ data: data })
+    const unauthorizedUser = await prisma.user.create({
+      data: unauthorizedUserdata
+    })
+
+    const token = generateAccessToken(unauthorizedUser)
+
+    const result = await app.inject({
+      method: 'PATCH',
+      path: `/users/${user.id}`,
+      payload: body,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    expect(result.statusCode).toBe(403)
+    expect(result.json().message).toBe("you don't have access rights")
+  })
+})
