8a82e3cb19c2f0103f5927b1d02374427d5c7b74
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-08-18 20:27:16 -0300

    docs: adicionar a rota de criar boards

--- src/app/modules/board/board.controller.ts
+++ src/app/modules/board/board.controller.ts
@@ -1,14 +1,29 @@
 import { JwtAuthGuard } from '@modules/auth/guards/JwtAuth.guard'
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common'
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiTags
+} from '@nestjs/swagger'
+import { BadRequestResponse } from '@src/app/docs/BadRequest.reponse'
 
 import { BoardService } from './board.service'
+import { BoardEntity } from './docs/board.entity'
 import { CreateBoardDto } from './dto/createBoard.dto'
 
+@ApiTags('Boards')
+@ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 @Controller('boards')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
+  @ApiCreatedResponse({ description: 'Created', type: BoardEntity })
+  @ApiBadRequestResponse({
+    description: 'Validation error',
+    type: BadRequestResponse
+  })
   @Post()
   create(@Body() dto: CreateBoardDto, @Request() req) {
     return this.boardService.create(req.user.id, dto)
--- src/app/modules/board/dto/createBoard.dto.ts
+++ src/app/modules/board/dto/createBoard.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { MaxLength } from 'class-validator'
 
 export class CreateBoardDto {
+  @ApiProperty({ maxLength: 30 })
   @MaxLength(30)
   title: string
 
+  @ApiProperty({ maxLength: 1500 })
   @MaxLength(1500)
   description: string
 }
