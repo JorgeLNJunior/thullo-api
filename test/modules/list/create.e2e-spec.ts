1ab0a97a5a1a290421419c5bd74599ce02ddcaf8
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-08-24 15:20:34 -0300

    test(e2e): criar rota para criar listas

---
+++ test/modules/list/create.e2e-spec.ts
@@ -0,0 +1,125 @@
+import { faker } from '@faker-js/faker'
+import { ListEntity } from '@modules/list/docs/list.entity'
+import { CreateListDto } from '@modules/list/dto/createList.dto'
+import { useContainer } from '@nestjs/class-validator'
+import { ValidationPipe } from '@nestjs/common'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppModule } from '@src/app.module'
+import { BoardBuilder } from '@test/builders/board.builder'
+import { MemberBuilder } from '@test/builders/member.builder'
+import { UserBuilder } from '@test/builders/user.builder'
+import { PrismaService } from 'nestjs-prisma'
+
+import { generateAccessToken } from '../../helpers/auth.helper'
+
+describe('ListController/create (e2e)', () => {
+  let app: NestFastifyApplication
+  let prisma: PrismaService
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter()
+    )
+    prisma = app.get(PrismaService)
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        forbidNonWhitelisted: true,
+        whitelist: true
+      })
+    )
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
+    await app.init()
+    await app.getHttpAdapter().getInstance().ready()
+  })
+
+  afterEach(async () => {
+    await app.close()
+  })
+
+  it('/boards/:boardId/lists (POST) Should add a list to a board', async () => {
+    const body: CreateListDto = {
+      title: faker.lorem.word()
+    }
+
+    const user = await new UserBuilder().persist(prisma)
+    const board = await new BoardBuilder().setOwner(user.id).persist(prisma)
+    await new MemberBuilder()
+      .setBoard(board.id)
+      .setUser(user.id)
+      .persist(prisma)
+
+    const token = generateAccessToken(user)
+
+    const result = await app.inject({
+      method: 'POST',
+      path: `/boards/${board.id}/lists`,
+      payload: body,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    expect(result.statusCode).toBe(201)
+    expect(result.json()).toMatchObject(ListEntity.prototype)
+  })
+
+  it('/boards/:boardId/lists (POST) Should return 404 if it receives an invalid board id', async () => {
+    const body: CreateListDto = {
+      title: faker.lorem.word()
+    }
+    const id = faker.datatype.uuid()
+
+    const user = await new UserBuilder().persist(prisma)
+
+    const token = generateAccessToken(user)
+
+    const result = await app.inject({
+      method: 'POST',
+      path: `/boards/${id}/lists`,
+      payload: body,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    expect(result.statusCode).toBe(404)
+    expect(result.json().message).toBe('board not found')
+  })
+
+  it('/boards/:boardId/lists (POST) Should return 400 if title is empty', async () => {
+    const body: CreateListDto = {
+      title: undefined
+    }
+
+    const user = await new UserBuilder().persist(prisma)
+    const board = await new BoardBuilder().setOwner(user.id).persist(prisma)
+    await new MemberBuilder()
+      .setBoard(board.id)
+      .setUser(user.id)
+      .persist(prisma)
+
+    const token = generateAccessToken(user)
+
+    const result = await app.inject({
+      method: 'POST',
+      path: `/boards/${board.id}/lists`,
+      payload: body,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    expect(result.statusCode).toBe(400)
+  })
+})
