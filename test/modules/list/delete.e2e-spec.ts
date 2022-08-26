54e1f6793c49714df139ab68286f2175dc49047d
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-08-26 18:25:49 -0300

    test(e2e): criar a rota de deletar listas

---
+++ test/modules/list/delete.e2e-spec.ts
@@ -0,0 +1,113 @@
+import { useContainer } from '@nestjs/class-validator'
+import { ValidationPipe } from '@nestjs/common'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppModule } from '@src/app.module'
+import { BoardBuilder } from '@test/builders/board.builder'
+import { ListBuilder } from '@test/builders/list.builder'
+import { MemberBuilder } from '@test/builders/member.builder'
+import { UserBuilder } from '@test/builders/user.builder'
+import { PrismaService } from 'nestjs-prisma'
+import { randomUUID } from 'node:crypto'
+
+import { generateAccessToken } from '../../helpers/auth.helper'
+
+describe('ListController/delete (e2e)', () => {
+  let app: NestFastifyApplication
+  let prisma: PrismaService
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter()
+    )
+    prisma = app.get(PrismaService)
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        forbidNonWhitelisted: true,
+        whitelist: true
+      })
+    )
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
+    await app.init()
+    await app.getHttpAdapter().getInstance().ready()
+  })
+
+  afterEach(async () => {
+    await app.close()
+  })
+
+  it('/lists (DELETE) Should delete a list', async () => {
+    const user = await new UserBuilder().persist(prisma)
+    const board = await new BoardBuilder().setOwner(user.id).persist(prisma)
+    await new MemberBuilder()
+      .setBoard(board.id)
+      .setUser(user.id)
+      .persist(prisma)
+    const list = await new ListBuilder().setBoard(board.id).persist(prisma)
+
+    const token = generateAccessToken(user)
+
+    const result = await app.inject({
+      method: 'DELETE',
+      path: `/boards/${board.id}/lists/${list.id}`,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    expect(result.statusCode).toBe(200)
+    expect(result.json().message).toBe('the list has been deleted')
+  })
+
+  it('/lists (DELETE) Should return 403 if the user is not a member of the board', async () => {
+    const user = await new UserBuilder().persist(prisma)
+    const board = await new BoardBuilder().setOwner(user.id).persist(prisma)
+    const list = await new ListBuilder().setBoard(board.id).persist(prisma)
+
+    const token = generateAccessToken(user)
+
+    const result = await app.inject({
+      method: 'DELETE',
+      path: `/boards/${board.id}/lists/${list.id}`,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    expect(result.statusCode).toBe(403)
+    expect(result.json().message).toBe('you are not a member of this board')
+  })
+
+  it('/lists (DELETE) Should return 404 if the list was not found', async () => {
+    const user = await new UserBuilder().persist(prisma)
+    const board = await new BoardBuilder().setOwner(user.id).persist(prisma)
+    await new MemberBuilder()
+      .setBoard(board.id)
+      .setUser(user.id)
+      .persist(prisma)
+    const id = randomUUID()
+
+    const token = generateAccessToken(user)
+
+    const result = await app.inject({
+      method: 'DELETE',
+      path: `/boards/${board.id}/lists/${id}`,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    expect(result.statusCode).toBe(404)
+    expect(result.json().message).toBe('list not found')
+  })
+})
