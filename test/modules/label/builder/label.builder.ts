f96d2119f77b03f9a6075fed8fba45c9068ac0a9
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-10-27 15:51:28 -0300

    test(e2e): adicionar rota para atualizar labels

---
+++ test/modules/label/builder/label.builder.ts
@@ -0,0 +1,53 @@
+import { randomUUID } from 'node:crypto'
+
+import { faker } from '@faker-js/faker'
+import { LabelColor } from '@modules/label/labelColor.enum'
+import { Label } from '@prisma/client'
+import { PrismaService } from 'nestjs-prisma'
+
+export class LabelBuilder {
+  private label: Label = {
+    id: randomUUID(),
+    boardId: randomUUID(),
+    title: faker.lorem.word(),
+    color: LabelColor.BLUE,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+
+  /**
+   * Build a new label.
+   * @returns A `Label`object.
+   */
+  build(): Label {
+    return this.label
+  }
+
+  /**
+   * Set the color of the label.
+   * @param color The label color.
+   */
+  setColor(color: LabelColor): LabelBuilder {
+    this.label.color = color
+    return this
+  }
+
+  /**
+   * Set the label board.
+   * @param id The id of the board.
+   */
+  setBoard(id: string): LabelBuilder {
+    this.label.boardId = id
+    return this
+  }
+
+  /**
+   * Persists the label in the database.
+   * @returns A `Label`object.
+   */
+  async persist(prisma: PrismaService): Promise<Label> {
+    return prisma.label.create({
+      data: this.label
+    })
+  }
+}
---
+++ test/modules/label/update.e2e-spec.ts
@@ -0,0 +1,82 @@
+import { faker } from '@faker-js/faker'
+import { LabelEntity } from '@modules/label/docs/label.entity'
+import { UpdateLabelDto } from '@modules/label/dto/updateLabel.dto'
+import { useContainer } from '@nestjs/class-validator'
+import { ValidationPipe } from '@nestjs/common'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppModule } from '@src/app.module'
+import { PrismaService } from 'nestjs-prisma'
+
+import { generateAccessToken } from '../auth/helpers/auth.helper'
+import { BoardBuilder } from '../board/builder/board.builder'
+import { MemberBuilder } from '../member/builder/member.builder'
+import { UserBuilder } from '../user/builder/user.builder'
+import { LabelBuilder } from './builder/label.builder'
+
+describe('BoardLabelController/update (e2e)', () => {
+  let app: NestFastifyApplication
+  let prisma: PrismaService
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter()
+    )
+    prisma = app.get(PrismaService)
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        forbidNonWhitelisted: true,
+        whitelist: true,
+        validatorPackage: require('@nestjs/class-validator'),
+        transformerPackage: require('@nestjs/class-transformer')
+      })
+    )
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
+    await app.init()
+    await app.getHttpAdapter().getInstance().ready()
+  })
+
+  afterEach(async () => {
+    await app.close()
+  })
+
+  it('/boards/:id/labels (PATCH) Should update a label', async () => {
+    const body: UpdateLabelDto = {
+      title: faker.lorem.words(1)
+    }
+
+    const user = await new UserBuilder().persist(prisma)
+    const board = await new BoardBuilder().setOwner(user.id).persist(prisma)
+    await new MemberBuilder()
+      .setBoard(board.id)
+      .setUser(user.id)
+      .persist(prisma)
+    const label = await new LabelBuilder().setBoard(board.id).persist(prisma)
+
+    const token = generateAccessToken(user)
+
+    const result = await app.inject({
+      method: 'PATCH',
+      path: `/boards/${board.id}/labels/${label.id}`,
+      payload: body,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    console.log(result.json())
+
+    expect(result.statusCode).toBe(200)
+    expect(result.json()).toMatchObject(LabelEntity.prototype)
+  })
+})
