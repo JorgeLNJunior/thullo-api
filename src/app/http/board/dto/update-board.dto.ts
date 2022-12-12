4241ab184f0148238ec72cdefd0ac43d4582fd4d
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-12-12 10:10:13 -0300

    feat: permitir que a visibilidade de um board seja alterada

--- src/app/http/board/dto/update-board.dto.ts
+++ src/app/http/board/dto/update-board.dto.ts
@@ -1,21 +1,27 @@
-import { IsOptional, MaxLength } from '@nestjs/class-validator'
+import { IsEnum, IsOptional, MaxLength } from '@nestjs/class-validator'
 import { ApiProperty } from '@nestjs/swagger'
+import { BoardVisibility } from '@prisma/client'
 
 import { IsUnsplashImageUrl } from '../decorators/isUnsplashImageUrl.decorator'
 
 export class UpdateBoardDto {
-  @ApiProperty({ maxLength: 30 })
+  @ApiProperty({ maxLength: 30, required: false })
   @MaxLength(30)
   @IsOptional()
   title?: string
 
-  @ApiProperty({ maxLength: 1500 })
+  @ApiProperty({ maxLength: 1500, required: false })
   @MaxLength(1500)
   @IsOptional()
   description?: string
 
-  @ApiProperty()
+  @ApiProperty({ required: false })
   @IsUnsplashImageUrl()
   @IsOptional()
   coverImage?: string
+
+  @ApiProperty({ enum: BoardVisibility, required: false })
+  @IsEnum(BoardVisibility)
+  @IsOptional()
+  visibility?: BoardVisibility
 }
